f0656533562f3e35b55a83cf0cae2511cbe0218a
sasu <sasuolander@yahoo.com>
2017-05-04 11:40:30 +0300

    SQL codes and jsp web pages

---
+++ Iieni(working title)/SQLfolder/sql_codes.sql
@@ -0,0 +1,11 @@
+CREATE TABLE CONTACT (
+ID int auto_increment primary key,
+FirstName varchar(30),
+LastName varchar(30),
+Title varchar(30),
+Organization varchar(30),
+Address varchar(30)
+ZIP code varchar(30)
+City varchar(30)
+Country varchar(30),
+);
